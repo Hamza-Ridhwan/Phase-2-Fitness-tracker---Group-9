3683e817f0e9e79a98a72928895748c26afeb94a
hamza-ridhwan <hamzathehamzah@gmail.com>
2024-11-14 12:32:20 +0300

    Updated footer links

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <a href="https://policies.google.com/privacy?hl=en"target="_blank">Privacy Policy</a>
-      <a href="https://policies.google.com/terms?hl=en-US"target="_blank">Terms of Service</a>
-      <a href="https://www.instagram.com/"target="_blank">Social Media</a>
+      <a href="https://policies.google.com/privacy?hl=en"target="_blank" rel="noreferrer">Privacy Policy</a>
+      <a href="https://policies.google.com/terms?hl=en-US"target="_blank" rel="noreferrer">Terms of Service</a>
+      <a href="https://www.instagram.com/"target="_blank" rel="noreferrer">Social Media</a>
       <p>Fitness Tracker &copy; 2024</p>
     </footer>
   );
